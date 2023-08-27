d3c47df36d68448f1b972cf8525207482a3db935
gunnar-miklis <gunnar.miklis@gmail.com>
2023-08-27 19:08:00 +0200

    refractor style guide values

---
+++ tip-calculator/solutions/react-mui/src/styleGuide.js
@@ -0,0 +1,16 @@
+export const styleGuide = {
+	font: {
+		fontFamily: [ 'Space Mono', 'sans-serif' ].join( ',' ),
+		fontWeight: 700,
+		fontSize: '24px',
+	},
+	colors: {
+		main: 'hsl(172, 67%, 45%)',
+		darkCyan: 'hsl(183, 100%, 15%)',
+		darkGrayishCyan: 'hsl(186, 14%, 43%)',
+		grayishCyan: 'hsl(184, 14%, 56%)',
+		lightGrayishCyan: 'hsl(185, 41%, 84%)',
+		lightCyan: 'hsl(189, 41%, 97%)',
+		white: 'hsl(0, 0%, 100%)',
+	},
+};
