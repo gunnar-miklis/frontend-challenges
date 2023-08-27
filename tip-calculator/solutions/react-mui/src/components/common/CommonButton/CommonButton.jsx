d5648b5121c4edb8f5f451f730733cc28a39932e
gunnar-miklis <gunnar.miklis@gmail.com>
2023-08-27 19:26:01 +0200

    YAGNI, KISS

--- tip-calculator/solutions/react-mui/src/components/common/CommonButton/CommonButton.jsx
+++ tip-calculator/solutions/react-mui/src/components/common/CommonButton/CommonButton.jsx
@@ -1,26 +1,12 @@
 import { Button } from '@mui/material';
-import { useEffect, useState } from 'react';
 import { styleGuide } from '../../../styleGuide';
 
 export default function CommonButton( { title, isActive, onClick, disabled } ) {
-	const [ active, setActive ] = useState( {} );
-
-	useEffect( () => {
-		if ( isActive ) {
-			setActive( {
-				backgroundColor: styleGuide.colors.main,
-				color: styleGuide.colors.darkCyan,
-			} );
-		} else {
-			setActive( {} );
-		}
-	}, [ isActive ] );
-
 	return (
 		<Button
 			variant='contained'
 			fullWidth
-			sx={active}
+			sx={ isActive ? { backgroundColor: styleGuide.colors.main, color: styleGuide.colors.darkCyan } : {} }
 			disabled={disabled}
 			onClick={onClick}
 		>
